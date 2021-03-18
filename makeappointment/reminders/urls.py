702bdaead2c7824ba3390d692562ef7f0b91c446
Sebastian Abarca <49495659+abrusebas1997@users.noreply.github.com>
2021-03-18 13:11:37 -0400

    Update urls.py

--- makeappointment/reminders/urls.py
+++ makeappointment/reminders/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls import re_path
 
 from .views import (
     AppointmentCreateView,
+    AppointmentListView,
+    AppointmentDeleteView,
 )
 
 urlpatterns = [
     # Create, update, delete
     re_path(r'^new$', AppointmentCreateView.as_view(), name='new_appointment'),
     re_path(r'^$', AppointmentListView.as_view(), name='list_appointments'),
+    re_path(r'^/(?P[0-9]+)/delete$', AppointmentDeleteView.as_view(), name='delete_appointment'),
+
 ]
