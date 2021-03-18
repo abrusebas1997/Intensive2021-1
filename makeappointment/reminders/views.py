2421c0da3c2482777e74561f84953cf8ad2e6b64
Sebastian Abarca <49495659+abrusebas1997@users.noreply.github.com>
2021-03-18 01:18:31 -0400

    added [Appointment View]
    Using Django generic CreateView to make the Appointment view.
    Using SuccessMessageMixin to display the message 'Appointment succesfully created' after a successful creation

--- makeappointment/reminders/views.py
+++ makeappointment/reminders/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import DeleteView
+from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Appointment
+
+
+class AppointmentCreateView(SuccessMessageMixin, CreateView):
+    """Powers a form to create a new Appointment"""
+
+    model = Appointment
+    fields = ['name', 'phone_number', 'time', 'time_zone']
+    success_message = 'Appointment succesfully created'
